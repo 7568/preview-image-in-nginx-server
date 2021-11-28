db4a52146b418fba48ca48d65373e4da39c37e30
7568 <18508227568@163.com>
2021-11-28 14:37:49 +0800

    add jquery draggable

--- getting-started/popup.js
+++ getting-started/popup.js
@@ -22,16 +22,27 @@ function setPageBackgroundColor() {
     // document.body.style.backgroundColor = color;
     if($('#preview_img').length>0){
       $('#preview_img').remove()
+      $('#draggable').remove()
       $('body pre').show()
     }else{
+      window_width = $(window).width()
       $('body pre').hide()
-      imgs='<div id="preview_img">'
+      imgs='<div id="draggable" style="display: none;"></div><div id="preview_img">'
       $('body a:gt(0)').each(function(){
-        imgs += "<div style='padding: 1px;width : 360px;float: left;text-align: center;';><img src='"+$(this).attr('href')+"' style='padding: 1px;width :360px;';>"+$(this).text()+"</div>";
+        imgs += "<div style='padding: 1px;max-width:19%;text-align: center;';><img src='"+$(this).attr('href')+"' style='padding: 1px;max-width :"+(window_width/5.8)+"px;';>"+$(this).text()+"</div>";
       })
       imgs +='</div>'
       $('body').append(imgs)
-      }
+      $( "#preview_img" ).sortable({
+        revert: true
+      });
+      $( "#draggable" ).draggable({
+        connectToSortable: "#preview_img",
+        helper: "clone",
+        revert: "invalid"
+      });
+      $('#preview_img div').css({'display':'inline-block'})
+    }
     
   });
 }
